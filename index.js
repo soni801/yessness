209c1c07d57a33dd2a73c8f1edbda86e139ff9df
soni801 <32430506+soni801@users.noreply.github.com>
2021-05-21 19:19:01 +0200

    Add text loading transition

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const links = document.querySelectorAll("a");
-
+const text = document.querySelectorAll("body > h1, body > p");
 let i = 0;
-function setVisible()
+
+function transitionLinks()
 {
     setTimeout(function()
     {
         links[i].style.opacity = "1";
         links[i].style.transform = "none";
         i++;
-        if (i < links.length) setVisible();
+        if (i < links.length) transitionLinks();
     }, 30);
 }
 
-setVisible();
+function transitionText()
+{
+    setTimeout(function()
+    {
+        text[i].style.opacity = "1";
+        i++;
+        if (i < text.length) transitionText();
+    }, 80);
+}
+
+transitionText();
+setTimeout(function ()
+{
+    i = 0;
+    transitionLinks();
+}, 500);
