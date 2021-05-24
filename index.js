2328ee0ac9a8caf676336cc1c7f36344f4da9c8f
soni801 <32430506+soni801@users.noreply.github.com>
2021-05-24 13:10:52 +0200

    Fix inconsistent tilt speed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function transitionLinks()
 {
     setTimeout(function()
     {
+        links[i].style.transition = "300ms ease";
         links[i].style.opacity = "1";
         links[i].style.transform = "perspective(1000px)";
         i++;
