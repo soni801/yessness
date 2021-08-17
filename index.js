0e024934c4ed4abc54c0e67cd290b399b6911cb1
soni801 <mail.soni801@gmail.com>
2021-08-17 14:06:38 +0200

    Improve page load animation

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-const welcome = document.querySelectorAll("#welcome h1");
 const links = document.querySelectorAll("a");
 const text = document.querySelectorAll("body > h1, body > p");
 let i = 0;
-
-function transitionWelcomeIn()
-{
-    setTimeout(function()
-    {
-        welcome[i].style.opacity = "1";
-        welcome[i].style.transform = "none";
-        i++;
-        if (i < text.length) transitionWelcomeIn();
-    }, 200);
-}
-
-function transitionWelcomeOut()
-{
-    setTimeout(function()
-    {
-        welcome[i].style.opacity = "0";
-        welcome[i].style.transform = "translate(0, -1rem)";
-        i++;
-        if (i < text.length) transitionWelcomeOut();
-    }, 200);
-}
 
 function transitionLinks()
 {
@@ -34,7 +11,7 @@ function transitionLinks()
         links[i].style.transform = "perspective(1000px)";
         i++;
         if (i < links.length) transitionLinks();
-    }, 30);
+    }, 40);
 }
 
 function transitionText()
@@ -47,22 +24,16 @@ function transitionText()
     }, 80);
 }
 
-transitionWelcomeIn();
-setTimeout(function ()
-{
-    i = 0;
-    transitionWelcomeOut();
-}, 1500);
 setTimeout(function ()
 {
     i = 0;
     transitionText();
-}, 2400);
+}, 200);
 setTimeout(function ()
 {
     i = 0;
     transitionLinks();
-}, 2800);
+}, 600);
 
 VanillaTilt.init(links,
     {
